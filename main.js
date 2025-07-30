d0e9aaaeb8dbd676c5389565c0851d8c03ced0f1
johnsonjacob298 <76007081+johnsonjacob298@users.noreply.github.com>
2025-07-29 21:01:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+let scrollContainer = document.querySelector('.gallery');
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
 
+scrollContainer.addEventListener('wheel', (e) => {
+    e.defaultPrevented();
+    scrollContainer.scrollLeft += e.deltaY;
+    scrollContainer.style.scrollBehavior = 'auto';
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = 'smooth';
+    scrollContainer.scrollLeft += 1000
+});
+
+prevBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = 'smooth';
+    scrollContainer.scrollLeft -= 1000;
+});

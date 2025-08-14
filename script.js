d4e773a43863e73beb16e483ca26d8355c9d63e7
johnsonjacob298 <76007081+johnsonjacob298@users.noreply.github.com>
2025-08-14 14:40:34 -0400

    476

--- script.js
+++ script.js
@@ -12,15 +12,3 @@ function initThemeToggle() {
     localStorage.setItem('theme', isLight ? 'light' : 'dark');
   });
 }
-
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then((registration) => {
-            console.log('Service Worker registered:', registration);
-          })
-          .catch((error) => {
-            console.error('Service Worker registration failed:', error);
-          });
-      });
-    }
